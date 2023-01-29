acefbe8fce203e5e90b64f432206d3a35aa1574f
Skajpik <lukyminecra@gmail.com>
2023-01-29 15:21:32 +0100

    Update ZipCracker.py

--- ZipCracker.py
+++ ZipCracker.py
@@ -113,7 +113,7 @@ def SelFile():
     folderCracked.reverse()
     folderCracked = folderCracked[0]
     dpfile = dpfile.replace('\\', '/').strip().strip('"').strip("'")
-    if folderCracked.split(".")[1] != 'zip': print("\n\033[1;91m Unknown file format, you can only use: \033[4;91m.zip\033[0;91m\n"); SelFile()
+    if folderCracked.split(".")[1].strip(" '") != 'zip': print("\n\033[1;91m Unknown file format, you can only use: \033[4;91m.zip\033[0;91m\n"); SelFile()
 SelFile()
 
 
